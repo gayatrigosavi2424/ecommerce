d6df915f8cd0f087c8acd653d3af193d993b6037
gayatrigosavi24 <gayatrigosavi9340@gmail.com>
2025-06-29 19:12:27 +0530

    Shop context modified-1

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { ShopContext } from '../context/Shopcontext';
+import { ShopContext } from '../context/ShopContext';
 import ProductItem from './ProductItem';
 import Title from './Title';
 
--- frontend/src/components/RelatedProduct.jsx
+++ frontend/src/components/RelatedProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/Shopcontext'
+import { ShopContext } from '../context/ShopContext'
 import { products } from '../assets/assets';
 import ProductItem from './ProductItem';
 import Title from './Title';
